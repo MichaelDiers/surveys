4dcd48631b212ce3dc6b0aed603aca6a8b026389
Michael Diers <md@sreid.de>
2022-01-07 23:43:17 +0100

    SaveSurveyService: pubsub added, build/deploy fix

--- backend/SaveSurveyService/functions/index.js
+++ backend/SaveSurveyService/functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const { PubSub } = require('@google-cloud/pubsub');
 
 admin.initializeApp();
 const database = admin.firestore();
@@ -7,9 +8,16 @@ const database = admin.firestore();
 const {
   collectionname: collectionName,
   topicname: topicName,
+  projectid: projectId,
+  topicnamecreated: topicNameCreated,
 } = functions.config().savesurveyservice;
+
+const pubsub = new PubSub({ projectId });
 
 exports.SaveSurvey = functions.pubsub.topic(topicName).onPublish(async (message) => {
   const docRef = database.collection(collectionName).doc();
   await docRef.set(message.json);
+
+  const data = Buffer.from(JSON.stringify(message.json));
+  await pubsub.topic(topicNameCreated).publishMessage({ data });
 });
