36adf717d61ae15123f31f5ac8b272b360fdfd3a
Michael Diers <md@sreid.de>
2022-01-13 01:37:12 +0100

    nodejs gcloud test

--- backend/SurveyTester/functions/index.js
+++ backend/SurveyTester/functions/index.js
@@ -17,12 +17,7 @@ const pubsub = new PubSub({ projectId });
 exports.SurveyTester = functions.https.onRequest(async (req, res) => {
   const docRef = database.collection(collectionName).doc(documentId);
   const doc = await docRef.get();
-  console.log(collectionName);
-  console.log(documentId);
-  console.log(process.env.ENV_COLLECTION_NAME);
-  console.log(process.env.ENV_DOCUMENT_ID);
-  console.log(doc);
-  console.log(doc.exists);
+
   if (doc.exists) {
     const data = doc.data();
 
