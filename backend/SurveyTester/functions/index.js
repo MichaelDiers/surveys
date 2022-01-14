282e39864ce1c680f7da5f423ec2c13a8f3af5c6
Michael Diers <md@sreid.de>
2022-01-14 14:13:13 +0100

    schema update, deploy update, tester update

--- backend/SurveyTester/functions/index.js
+++ backend/SurveyTester/functions/index.js
@@ -3,7 +3,7 @@ const { PubSub } = require('@google-cloud/pubsub');
 
 const {
   ENV_COLLECTION_NAME: collectionName = 'test-surveys',
-  ENV_DOCUMENT_ID: documentId = 'test-survey',
+  ENV_DOCUMENT_ID: documentId = 'survey-tester-input',
   ENV_TOPIC_NAME_PUB: topicNamePub = 'SAVE_SURVEY',
 } = process.env;
 
@@ -32,7 +32,8 @@ exports.SurveyTester = async (req, res) => {
   const doc = await docRef.get();
 
   if (doc.exists) {
-    const data = Buffer.from(orderedStringify(doc.data()));      
+    const stringify = orderedStringify(doc.data());
+    const data = Buffer.from(stringify);
     await pubsub.topic(topicNamePub).publishMessage({ data });
     res.send('OK').end();
   } else {
