5d0a71b371c4359c7f0f1dac7f5de33d804141e0
Michael Diers <md@sreid.de>
2022-01-14 02:25:22 +0100

    SurveyTester: schema validation, deployment

--- backend/SurveyTester/functions/index.js
+++ backend/SurveyTester/functions/index.js
@@ -2,23 +2,38 @@ const Firestore = require('@google-cloud/firestore');
 const { PubSub } = require('@google-cloud/pubsub');
 
 const {
-  ENV_COLLECTION_NAME: collectionName,
-  ENV_DOCUMENT_ID: documentId,
-  ENV_TOPIC_NAME_PUB: topicNamePub,
+  ENV_COLLECTION_NAME: collectionName = 'test-surveys',
+  ENV_DOCUMENT_ID: documentId = 'test-survey',
+  ENV_TOPIC_NAME_PUB: topicNamePub = 'SAVE_SURVEY',
 } = process.env;
 
 const database = new Firestore();
 const pubsub = new PubSub();
+
+// https://stackoverflow.com/questions/16167581/sort-object-properties-and-json-stringify
+const orderedStringify = (obj) => {
+  const allKeys = [];
+  const seen = {};
+  JSON.stringify(obj, (key, value) => {
+    if (!(key in seen)) {
+      allKeys.push(key);
+      seen[key] = null;
+    }
+
+    return value;
+  });
+
+  allKeys.sort();
+  return JSON.stringify(obj, allKeys);
+};
 
 exports.SurveyTester = async (req, res) => {
   const docRef = database.collection(collectionName).doc(documentId);
   const doc = await docRef.get();
 
   if (doc.exists) {
-    const data = doc.data();
-
-    const buffer = Buffer.from(JSON.stringify(data));
-    await pubsub.topic(topicNamePub).publishMessage({ data: buffer });
+    const data = Buffer.from(orderedStringify(doc.data()));
+    await pubsub.topic(topicNamePub).publishMessage({ data });
     res.send('OK').end();
   } else {
     res.send('no data').end();
