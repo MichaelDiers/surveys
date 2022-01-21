14e9a8b405f538262e730b7c07e2f88dbfec95a9
Michael Diers <md@sreid.de>
2022-01-21 01:31:35 +0100

    SurveyViewerService: json names

--- backend/SurveyViewerService/SurveyViewerService/Model/SurveyViewData.cs
+++ backend/SurveyViewerService/SurveyViewerService/Model/SurveyViewData.cs
@@ -1,6 +1,7 @@
 ﻿namespace SurveyViewerService.Model
 {
 	using System.Collections.Generic;
+	using Newtonsoft.Json;
 
 	/// <summary>
 	///   Survey data that is used in frontend context.
@@ -10,26 +11,31 @@
 		/// <summary>
 		///   Gets or sets a value that indicates if the survey is closed.
 		/// </summary>
+		[JsonProperty("isClosed")]
 		public bool IsClosed { get; set; }
 
 		/// <summary>
 		///   Gets or sets the id of the participant.
 		/// </summary>
+		[JsonProperty("participantId")]
 		public string ParticipantId { get; set; }
 
 		/// <summary>
 		///   Gets or sets the name of the participant.
 		/// </summary>
+		[JsonProperty("participantName")]
 		public string ParticipantName { get; set; }
 
 		/// <summary>
 		///   Gets or sets the questions of the survey.
 		/// </summary>
+		[JsonProperty("questions")]
 		public IEnumerable<SurveyViewDataQuestion> Questions { get; set; }
 
 		/// <summary>
 		///   Gets or sets the name of the survey.
 		/// </summary>
+		[JsonProperty("surveyName")]
 		public string SurveyName { get; set; }
 	}
 }
--- backend/SurveyViewerService/SurveyViewerService/Model/SurveyViewDataChoice.cs
+++ backend/SurveyViewerService/SurveyViewerService/Model/SurveyViewDataChoice.cs
@@ -1,5 +1,7 @@
 ﻿namespace SurveyViewerService.Model
 {
+	using Newtonsoft.Json;
+
 	/// <summary>
 	///   Describes a choice of a survey question.
 	/// </summary>
@@ -8,16 +10,19 @@
 		/// <summary>
 		///   Gets or sets a value that indicates if the value is selected.
 		/// </summary>
+		[JsonProperty("isSelected")]
 		public bool IsSelected { get; set; }
 
 		/// <summary>
 		///   Gets or sets the text of the choice.
 		/// </summary>
+		[JsonProperty("text")]
 		public string Text { get; set; }
 
 		/// <summary>
 		///   Gets the value of the choice.
 		/// </summary>
+		[JsonProperty("value")]
 		public string Value { get; set; }
 	}
 }
