3826e5019864960a16d1b1d66f865372e5d208e6
Michael Diers <md@sreid.de>
2022-01-28 17:31:05 +0100

    SurveyViewer: url fix

--- frontend/SurveyViewer/functions/app/controllers/participate.js
+++ frontend/SurveyViewer/functions/app/controllers/participate.js
@@ -64,7 +64,7 @@ const initialize = (config = {}) => {
     },
     viewSurveyAjax: async function viewSurveyAjax(participantId) {
       const url2 = `${surveyViewerServiceUrl}${participantId}`;
-      const client = await auth.getIdTokenClient(url2);
+      const client = await auth.getIdTokenClient('https://us-central1-surveys-services-test.cloudfunctions.net');
       const res = await client.request({url: url2});
       console.info(res.data);
 
