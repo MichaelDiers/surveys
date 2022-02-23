f9f4e19f21e95c41134f2b74aebad9538d915eb9
Michael Diers <md@sreid.de>
2022-02-23 22:54:34 +0100

    Surveys.Common: template reader

---
+++ nugets/Surveys.Common/Surveys.Common/Contracts/Mails/ITemplateProcessor.cs
@@ -0,0 +1,20 @@
+﻿namespace Surveys.Common.Contracts.Mails
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    ///     Process templates and input to create an email.
+    /// </summary>
+    public interface ITemplateProcessor
+    {
+        /// <summary>
+        ///     Process the input data and create an email.
+        /// </summary>
+        /// <typeparam name="T">The type of the input data.</typeparam>
+        /// <param name="templates">The email template data.</param>
+        /// <param name="input">The email content.</param>
+        /// <returns>A <see cref="Task" />.</returns>
+        Task ProcessAsync<T>(IDictionary<string, string> templates, T input) where T : class;
+    }
+}
