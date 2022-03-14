ac01ec3f5f4a12f2783842ea497ea76d0e3bbca1
MichaelDiers <md@sreid.de>
2022-03-14 20:18:21 +0100

    SurveyFrame: logging

--- frontend/SurveyFrame/application/controllers/index-controller.js
+++ frontend/SurveyFrame/application/controllers/index-controller.js
@@ -11,6 +11,12 @@ const initialize = (config = {}) => {
 
   const controller = {
     index: async (req, res) => {
+      console.log(req.originalUrl); // eslint-disable-line
+      console.log(req.baseUrl); // eslint-disable-line
+      console.log(req.path); // eslint-disable-line
+      console.log(req.originalUrl.split('/frame')); // eslint-disable-line
+      console.log(gatewayAddress); // eslint-disable-line
+
       const destination = req.originalUrl.split('/frame')[1];
       const placeholder = `${gatewayAddress}${destination}`;
       res.render('index/index', { placeholder });
