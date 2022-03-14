24c96720896fdc697c2cc0cb64c501819f40331c
MichaelDiers <md@sreid.de>
2022-03-14 20:09:29 +0100

    SurveyFrame: client js fix

--- frontend/SurveyFrame/client/js/sub-views.js
+++ frontend/SurveyFrame/client/js/sub-views.js
@@ -3,7 +3,7 @@ const subViews = (sourceElement) => {
   if (elements) {
     elements.forEach((element) => {
       const url = element.getAttribute('placeholder');
-      element.removeAttribute('placeholder');
+      // element.removeAttribute('placeholder');
       // eslint-disable-next-line no-undef
       apiCall(url, 'GET').then((result) => {
         const htmlElement = element;
