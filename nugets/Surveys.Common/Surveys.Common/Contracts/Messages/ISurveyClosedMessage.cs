4bfec7b9ce80f01ffd8b0427f37a891cdc9b7d03
MichaelDiers <md@sreid.de>
2022-03-20 13:57:17 +0100

    Surveys.Common: end of survey message

---
+++ nugets/Surveys.Common/Surveys.Common/Contracts/Messages/ISurveyClosedMessage.cs
@@ -0,0 +1,21 @@
+﻿namespace Surveys.Common.Contracts.Messages
+{
+    using System.Collections.Generic;
+    using Md.GoogleCloud.Base.Contracts.Messages;
+
+    /// <summary>
+    ///     Describes a closed survey message.
+    /// </summary>
+    public interface ISurveyClosedMessage : IMessage
+    {
+        /// <summary>
+        ///     Gets the survey results.
+        /// </summary>
+        IEnumerable<ISurveyResult> Results { get; }
+
+        /// <summary>
+        ///     Gets the survey.
+        /// </summary>
+        ISurvey Survey { get; }
+    }
+}
---
+++ nugets/Surveys.Common/Surveys.Common/Messages/SurveyClosedMessage.cs
@@ -0,0 +1,42 @@
+﻿namespace Surveys.Common.Messages
+{
+    using System.Collections.Generic;
+    using Md.GoogleCloud.Base.Messages;
+    using Newtonsoft.Json;
+    using Surveys.Common.Contracts;
+    using Surveys.Common.Contracts.Messages;
+    using Surveys.Common.Models;
+
+    /// <summary>
+    ///     Describes a closed survey message.
+    /// </summary>
+    public class SurveyClosedMessage : Message, ISurveyClosedMessage
+    {
+        /// <summary>
+        ///     Creates a new <see cref="SurveyClosedMessage" />.
+        /// </summary>
+        /// <param name="processId">The global process id.</param>
+        /// <param name="survey">The survey data.</param>
+        /// <param name="results">The survey results.</param>
+        [JsonConstructor]
+        public SurveyClosedMessage(string processId, Survey survey, IEnumerable<SurveyResult> results)
+            : base(processId)
+        {
+            this.Survey = survey;
+            this.Results = results;
+        }
+
+
+        /// <summary>
+        ///     Gets the survey results.
+        /// </summary>
+        [JsonProperty("results", Required = Required.Always, Order = 12)]
+        public IEnumerable<ISurveyResult> Results { get; }
+
+        /// <summary>
+        ///     Gets the survey.
+        /// </summary>
+        [JsonProperty("survey", Required = Required.Always, Order = 11)]
+        public ISurvey Survey { get; }
+    }
+}
