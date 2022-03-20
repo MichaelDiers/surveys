3470f61dc712dea92749ff6f556cb69e71cb4d03
MichaelDiers <md@sreid.de>
2022-03-21 00:10:00 +0100

    EvaluateSurveySubscriber: fix for order

--- backend/EvaluateSurveySubscriber/EvaluateSurveySubscriber.Tests/FunctionTests.cs
+++ backend/EvaluateSurveySubscriber/EvaluateSurveySubscriber.Tests/FunctionTests.cs
@@ -11,8 +11,10 @@
     using EvaluateSurveySubscriber.Tests.Mocks;
     using Google.Cloud.Functions.Testing;
     using Google.Events.Protobuf.Cloud.PubSub.V1;
+    using Md.GoogleCloud.Base.Logic;
     using Newtonsoft.Json;
     using Surveys.Common.Contracts.Messages;
+    using Surveys.Common.Messages;
     using Xunit;
 
     /// <summary>
@@ -33,26 +35,15 @@
             var message = TestData.CreateMessage();
             await HandleAsyncForMessageWithProvider(message);
         }
-        /*
+
         [Fact(Skip = "Integration")]
         public async void HandleAsyncWithProviderAndDatabase()
         {
-            var message = TestData.CreateMessage();
-            await HandleAsyncForMessageWithProvider(
-                message,
-                config => new PubSubClientMock(),
-                config => new ReadonlyDatabase(config));
+            var message = new EvaluateSurveyMessage(Guid.NewGuid().ToString(), "9a8f40a6-75b1-4351-90cc-7289db972760");
+
+            await HandleAsyncIntegration(message);
         }
 
-        [Fact(Skip = "Integration")]
-        public async void HandleAsyncWithProviderAndDatabaseAndPubSubClient()
-        {
-            var message = TestData.CreateMessage();
-            await HandleAsyncForMessageWithProvider(
-                message,
-                config => new PubSubClient(config),
-                config => new ReadonlyDatabase(config));
-        }*/
 
         private static async Task HandleAsyncForMessage(IEvaluateSurveyMessage message)
         {
@@ -77,7 +68,6 @@
         }
 
         private static async Task HandleAsyncForMessageWithProvider(IEvaluateSurveyMessage message)
-
         {
             var configuration =
                 JsonConvert.DeserializeObject<FunctionConfiguration>(await File.ReadAllTextAsync("appsettings.json"));
@@ -102,6 +92,42 @@
                 new SurveyStatusDatabaseMock(false),
                 new PubSubClientMock(),
                 new PubSubClientMock());
+            var function = new Function(logger, provider);
+            await function.HandleAsync(cloudEvent, data, CancellationToken.None);
+
+            Assert.Empty(logger.ListLogEntries());
+        }
+
+        private static async Task HandleAsyncIntegration(IEvaluateSurveyMessage message)
+        {
+            var configuration =
+                JsonConvert.DeserializeObject<FunctionConfiguration>(await File.ReadAllTextAsync("appsettings.json"));
+            Assert.NotNull(configuration);
+            var json = JsonConvert.SerializeObject(message);
+            var data = new MessagePublishedData {Message = new PubsubMessage {TextData = json}};
+
+            var cloudEvent = new CloudEvent
+            {
+                Type = MessagePublishedData.MessagePublishedCloudEventType,
+                Source = new Uri("//pubsub.googleapis.com", UriKind.RelativeOrAbsolute),
+                Id = Guid.NewGuid().ToString(),
+                Time = DateTimeOffset.UtcNow,
+                Data = data
+            };
+
+            var logger = new MemoryLogger<Function>();
+            var provider = new FunctionProvider(
+                logger,
+                new SurveyDatabase(
+                    new DatabaseConfiguration(configuration.ProjectId, configuration.SurveysCollectionName)),
+                new SurveyResultsDatabase(
+                    new DatabaseConfiguration(configuration.ProjectId, configuration.SurveyResultsCollectionName)),
+                new SurveyStatusDatabase(
+                    new DatabaseConfiguration(configuration.ProjectId, configuration.SurveyStatusCollectionName)),
+                new SaveSurveyStatusPubSubClient(
+                    new PubSubClientConfiguration(configuration.ProjectId, configuration.SaveSurveyStatusTopicName)),
+                new SurveyClosedPubSubClient(
+                    new PubSubClientConfiguration(configuration.ProjectId, configuration.SurveyClosedTopicName)));
             var function = new Function(logger, provider);
             await function.HandleAsync(cloudEvent, data, CancellationToken.None);
 
