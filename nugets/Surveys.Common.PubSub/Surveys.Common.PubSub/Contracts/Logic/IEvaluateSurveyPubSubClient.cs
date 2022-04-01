99cb75bf26a757dbf6966e797f9791e61a51a160
MichaelDiers <md@sreid.de>
2022-04-02 01:38:52 +0200

    Surveys.Common.PubSub: evaluate survey

---
+++ nugets/Surveys.Common.PubSub/Surveys.Common.PubSub/Contracts/Logic/IEvaluateSurveyPubSubClient.cs
@@ -0,0 +1,19 @@
+﻿namespace Surveys.Common.PubSub.Contracts.Logic
+{
+    using System.Threading.Tasks;
+    using Surveys.Common.Contracts.Messages;
+
+    /// <summary>
+    ///     Google pub/sub client for publishing an <see cref="IEvaluateSurveyMessage" />.
+    /// </summary>
+    public interface IEvaluateSurveyPubSubClient
+    {
+        /// <summary>
+        ///     Publish a <see cref="IEvaluateSurveyMessage" /> message.
+        /// </summary>
+        /// <param name="message">The message to publish.</param>
+        /// <returns>A <see cref="Task" /> that indicates completion.</returns>
+        // ReSharper disable once UnusedMember.Global
+        Task PublishAsync(IEvaluateSurveyMessage message);
+    }
+}
---
+++ nugets/Surveys.Common.PubSub/Surveys.Common.PubSub/Logic/EvaluateSurveyPubSubClient.cs
@@ -0,0 +1,21 @@
+﻿namespace Surveys.Common.PubSub.Logic
+{
+    using Md.GoogleCloudPubSub.Logic;
+    using Surveys.Common.Contracts.Messages;
+    using Surveys.Common.PubSub.Contracts.Logic;
+
+    /// <summary>
+    ///     Google pub/sub client for publishing an <see cref="IEvaluateSurveyMessage" />.
+    /// </summary>
+    public class EvaluateSurveyPubSubClient : AbstractPubSubClient<IEvaluateSurveyMessage>, IEvaluateSurveyPubSubClient
+    {
+        /// <summary>
+        ///     Creates a new instance of <see cref="IEvaluateSurveyPubSubClient" />.
+        /// </summary>
+        /// <param name="environment"></param>
+        public EvaluateSurveyPubSubClient(IPubSubClientEnvironment environment)
+            : base(environment)
+        {
+        }
+    }
+}
