0a85c619ad1281f788fab833e9e0d5d5e7b422a9
MichaelDiers <md@sreid.de>
2022-04-01 13:34:04 +0200

    Surveys.Common.PubSub: handle messages

---
+++ nugets/Surveys.Common.PubSub/Surveys.Common.PubSub/Logic/InitializeSurveyPubSubClient.cs
@@ -0,0 +1,22 @@
+﻿namespace Surveys.Common.PubSub.Logic
+{
+    using Md.GoogleCloudPubSub.Logic;
+    using Surveys.Common.Contracts;
+    using Surveys.Common.PubSub.Contracts.Logic;
+
+    /// <summary>
+    ///     Google pub/sub client for publishing an <see cref="IInitializeSurveyMessage" />.
+    /// </summary>
+    public class InitializeSurveyPubSubClient
+        : AbstractPubSubClient<IInitializeSurveyMessage>, IInitializeSurveyPubSubClient
+    {
+        /// <summary>
+        ///     Creates a new instance of <see cref="InitializeSurveyPubSubClient" />.
+        /// </summary>
+        /// <param name="environment"></param>
+        public InitializeSurveyPubSubClient(IPubSubClientEnvironment environment)
+            : base(environment)
+        {
+        }
+    }
+}
