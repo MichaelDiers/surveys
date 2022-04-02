c00c81555625057ac789ec1aa442f64680dc0bf1
MichaelDiers <md@sreid.de>
2022-04-02 08:41:59 +0200

    Surveys.Common.PubSub: handle send mail

---
+++ nugets/Surveys.Common.PubSub/Surveys.Common.PubSub/Contracts/Logic/ISendMailPubSubClient.cs
@@ -0,0 +1,19 @@
+﻿namespace Surveys.Common.PubSub.Contracts.Logic
+{
+    using System.Threading.Tasks;
+    using Surveys.Common.Contracts.Messages;
+
+    /// <summary>
+    ///     Google pub/sub client for publishing an <see cref="ISendMailMessage" />.
+    /// </summary>
+    public interface ISendMailPubSubClient
+    {
+        /// <summary>
+        ///     Publish a <see cref="ISendMailMessage" /> message.
+        /// </summary>
+        /// <param name="message">The message to publish.</param>
+        /// <returns>A <see cref="Task" /> that indicates completion.</returns>
+        // ReSharper disable once UnusedMember.Global
+        Task PublishAsync(ISendMailMessage message);
+    }
+}
---
+++ nugets/Surveys.Common.PubSub/Surveys.Common.PubSub/Logic/SendMailPubSubClient.cs
@@ -0,0 +1,21 @@
+﻿namespace Surveys.Common.PubSub.Logic
+{
+    using Md.GoogleCloudPubSub.Logic;
+    using Surveys.Common.Contracts.Messages;
+    using Surveys.Common.PubSub.Contracts.Logic;
+
+    /// <summary>
+    ///     Google pub/sub client for publishing an <see cref="ISendMailMessage" />.
+    /// </summary>
+    public class SendMailPubSubClient : AbstractPubSubClient<ISendMailMessage>, ISendMailPubSubClient
+    {
+        /// <summary>
+        ///     Creates a new instance of <see cref="CreateMailPubSubClient" />.
+        /// </summary>
+        /// <param name="environment"></param>
+        public SendMailPubSubClient(IPubSubClientEnvironment environment)
+            : base(environment)
+        {
+        }
+    }
+}
