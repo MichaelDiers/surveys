d41d814907dfb721e5ab1dc6d8404b3895f73b5a
MichaelDiers <md@sreid.de>
2022-04-06 17:04:46 +0200

    add survey closed client

---
+++ nugets/Surveys.Common.PubSub/Surveys.Common.PubSub/Contracts/Logic/ISurveyClosedPubSubClient.cs
@@ -0,0 +1,20 @@
+﻿namespace Surveys.Common.PubSub.Contracts.Logic
+{
+    using System.Threading.Tasks;
+    using Surveys.Common.Contracts;
+    using Surveys.Common.Contracts.Messages;
+
+    /// <summary>
+    ///     Google pub/sub client for publishing an <see cref="ISurveyClosedMessage" />.
+    /// </summary>
+    public interface ISurveyClosedPubSubClient
+    {
+        /// <summary>
+        ///     Publish a <see cref="ISaveSurveyMessage" /> message.
+        /// </summary>
+        /// <param name="message">The message to publish.</param>
+        /// <returns>A <see cref="Task" /> that indicates completion.</returns>
+        // ReSharper disable once UnusedMember.Global
+        Task PublishAsync(ISurveyClosedMessage message);
+    }
+}
---
+++ nugets/Surveys.Common.PubSub/Surveys.Common.PubSub/Logic/SurveyClosedPubSubClient.cs
@@ -0,0 +1,21 @@
+﻿namespace Surveys.Common.PubSub.Logic
+{
+    using Md.GoogleCloudPubSub.Logic;
+    using Surveys.Common.Contracts.Messages;
+    using Surveys.Common.PubSub.Contracts.Logic;
+
+    /// <summary>
+    ///     Google pub/sub client for publishing an <see cref="ISurveyClosedMessage" />.
+    /// </summary>
+    public class SurveyClosedPubSubClient : AbstractPubSubClient<ISurveyClosedMessage>, ISurveyClosedPubSubClient
+    {
+        /// <summary>
+        ///     Creates a new instance of <see cref="SaveSurveyPubSubClient" />.
+        /// </summary>
+        /// <param name="environment"></param>
+        public SurveyClosedPubSubClient(IPubSubClientEnvironment environment)
+            : base(environment)
+        {
+        }
+    }
+}
