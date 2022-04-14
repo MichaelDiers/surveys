925b6d2978bb7467f61029cb0d9225840dc1b4b8
MichaelDiers <md@sreid.de>
2022-04-14 11:07:27 +0200

    save survey messages changed

--- nugets/Surveys.Common/Surveys.Common/Contracts/ISaveSurveyMessage.cs
+++ nugets/Surveys.Common/Surveys.Common/Contracts/ISaveSurveyMessage.cs
@@ -8,11 +8,6 @@
     public interface ISaveSurveyMessage : IMessage
     {
         /// <summary>
-        ///     Gets the internal id of the survey.
-        /// </summary>
-        string InternalSurveyId { get; }
-
-        /// <summary>
         ///     Gets the data of the survey.
         /// </summary>
         ISurvey Survey { get; }
--- nugets/Surveys.Common/Surveys.Common/Messages/SaveSurveyMessage.cs
+++ nugets/Surveys.Common/Surveys.Common/Messages/SaveSurveyMessage.cs
@@ -1,7 +1,6 @@
 ﻿namespace Surveys.Common.Messages
 {
     using System;
-    using Md.Common.Extensions;
     using Md.Common.Messages;
     using Newtonsoft.Json;
     using Surveys.Common.Contracts;
@@ -15,37 +14,24 @@
         /// <summary>
         ///     Creates a new instance of <see cref="SaveSurveyMessage" />.
         /// </summary>
-        /// <param name="survey">The data of the survey.</param>
-        /// <param name="internalSurveyId">THe internal id of the survey.</param>
         /// <param name="processId">The id of the process.</param>
-        /// <exception cref="ArgumentNullException">Is thrown if <paramref name="survey" /> is null.</exception>
-        /// <exception cref="ArgumentException">It thrown if <paramref name="internalSurveyId" /> is null or whitespace.</exception>
+        /// <param name="survey">The data of the survey.</param>
         [JsonConstructor]
-        public SaveSurveyMessage(Survey survey, string internalSurveyId, string processId)
-            : this(survey as ISurvey, internalSurveyId, processId)
+        public SaveSurveyMessage(string processId, Survey survey)
+            : this(processId, survey as ISurvey)
         {
         }
 
         /// <summary>
         ///     Creates a new instance of <see cref="SaveSurveyMessage" />.
         /// </summary>
-        /// <param name="survey">The data of the survey.</param>
-        /// <param name="internalSurveyId">THe internal id of the survey.</param>
         /// <param name="processId">The id of the process.</param>
-        /// <exception cref="ArgumentNullException">Is thrown if <paramref name="survey" /> is null.</exception>
-        /// <exception cref="ArgumentException">It thrown if <paramref name="internalSurveyId" /> is null or whitespace.</exception>
-        public SaveSurveyMessage(ISurvey survey, string internalSurveyId, string processId)
+        /// <param name="survey">The data of the survey.</param>
+        public SaveSurveyMessage(string processId, ISurvey survey)
             : base(processId)
         {
             this.Survey = survey ?? throw new ArgumentNullException(nameof(survey));
-            this.InternalSurveyId = internalSurveyId.ValidateIsAGuid(nameof(internalSurveyId));
         }
-
-        /// <summary>
-        ///     Gets the internal id of the survey.
-        /// </summary>
-        [JsonProperty("internalSurveyId", Required = Required.Always, Order = 11)]
-        public string InternalSurveyId { get; }
 
         /// <summary>
         ///     Gets the data of the survey.
