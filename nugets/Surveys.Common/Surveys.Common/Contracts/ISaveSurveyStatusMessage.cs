da65035afbccb27d3dcef3dd7f50bf392059ab57
MichaelDiers <md@sreid.de>
2022-04-22 22:48:03 +0200

    interface fix

--- nugets/Surveys.Common/Surveys.Common/Contracts/ISaveSurveyStatusMessage.cs
+++ nugets/Surveys.Common/Surveys.Common/Contracts/ISaveSurveyStatusMessage.cs
@@ -1,12 +1,18 @@
 ﻿namespace Surveys.Common.Contracts
 {
     using Md.Common.Contracts.Messages;
+    using Surveys.Common.Contracts.Messages;
 
     /// <summary>
     ///     Save a new status of a survey.
     /// </summary>
     public interface ISaveSurveyStatusMessage : IMessage
     {
+        /// <summary>
+        ///     Gets the survey status data.
+        /// </summary>
+        ISurveyClosedMessage SurveyClosedMessage { get; }
+
         /// <summary>
         ///     Gets the survey status data.
         /// </summary>
